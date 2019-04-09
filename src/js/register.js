0c7579e62d9f74cffa9ce838b2f0475cb344fb9a
yougexiaoluo <1019134495@qq.com>
2019-04-09 19:57:12 +0800

    修改了编写代码时，粗心带来的多余标点符,以及在ie下不支持Array.form()方法

--- src/js/register.js
+++ src/js/register.js
@@ -1,4 +1,4 @@
-  var divLis = Array.from(document.querySelectorAll('form > div'));
+  var divLis = document.querySelectorAll('form > div');
   var arr = [23, 23, 24, 26, 20, 18, 27, 26, 30];
   for (var i = 0, len = arr.length; i < len; i++) {
     divLis[i].style['margin-bottom'] = (arr[i] / 10) + 'rem';
